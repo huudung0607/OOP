b8cc53f7621e995ead18f5a4c9d5d07a8bff945e
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-19 19:31:40 +0700

    Update Chuẩn hóa tên.cpp

--- Chuẩn hóa tên.cpp
+++ Chuẩn hóa tên.cpp
@@ -34,3 +34,41 @@ public:
         if(ns[1] == '/') ns = "0" + ns;
         if(ns[4] == '/') ns.insert(3, "0");
     }
+void in(){
+        cout << ma << " " << ten << " " << ns << " " << lop << " " << fixed << setprecision(2) << gpa << endl;
+    }
+    double getGpa(){
+        return gpa;
+    }
+    string getMa(){
+        return ma;
+    }
+};
+
+bool cmp(SV x, SV y){
+    if(x.getGpa() != y.getGpa())
+        return x.getGpa() > y.getGpa();
+    return x.getMa() < y.getMa();
+}
+
+int main(){
+    vector<SV> v;
+    int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        cin.ignore();
+        string ma, ten, ns, lop; double gpa;
+        getline(cin, ma);
+        getline(cin, ten);
+        getline(cin, ns);
+        getline(cin, lop);
+        cin >> gpa;
+        SV s(ma, ten, ns, lop, gpa);
+        s.chuanHoaTen();
+        s.chuanHoaNS();
+        v.push_back(s);
+    }
+    sort(v.begin(), v.end(), cmp);
+    for(SV x : v){
+        x.in();
+    }
+}
