c5ee47e37ca8441b0ddbe2c592273843a31c5ef3
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-01-21 20:37:53 +0700

    Create Tìm kiếm xe.cpp

---
+++ Tìm kiếm xe.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <stack>
+using namespace std;
+class vehicle {
+private:
+	string ma, ten, hang, color;
+public:
+	vehicle() {}
+	vehicle(string ma, string ten, string hang, string color) {
+		this->ma = ma;
+		this->ten = ten;
+		this->hang = hang;
+		this->color = color;
+	}
+	string getMa()
+	{
+		return ma;
+	}
+	string getTen()
+	{
+		return ten;
+	}
+	string getHang()
+	{
+		return hang;
+	}
+	string getColor()
+	{
+		return color;
+	}
+};
+class motorbike : public vehicle {
+private:
+	int velocity, gia;
+public:
+	motorbike() {}
+	motorbike(string ma, string ten, string hang, string color, int velocity, int gia) : vehicle(ma, ten, hang, color) {
+		this->velocity = velocity;
+		this->gia = gia;
+	}
+	void in() {
+		cout << getMa() << " " << getTen() << " " << getHang() << " " << getColor() << " " << velocity << " " << gia << endl;
+	}
+	int getGia()
+	{
+		return gia;
+	}
+};
+class car : public vehicle {
+private:
+	int hp, gia;
+public:
+	car() {}
+	car(string ma, string ten, string hang, string color, int hp, int gia) : vehicle(ma, ten, hang, color)
+	{
+		this->hp = hp;
+		this->gia = gia;
+	}
+	void in()
+	{
+		cout << getMa() << " " << getTen() << " " << getHang() << " " << getColor() << " " << hp << " " << gia << endl;
+	}
+	int getGia()
+	{
+		return gia;
+	}
+};
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	vector<motorbike> xemay;
+	vector<car> xehoi;
+	for (int i = 0; i < n; i++)
+	{
+		cin.ignore();
+		string ma, ten, hang, color;
+		int hp, gia, velocity;
+		getline(cin, ma);
+		getline(cin, ten);
+		getline(cin, hang);
+		getline(cin, color);
+		if (ma[0] == 'X')
+		{
+			cin >> velocity;
+			cin >> gia;
+			motorbike moto(ma, ten, hang, color, velocity, gia);
+			xemay.push_back(moto);
+		}
+		else
+		{
+			cin >> hp;
+			cin >> gia;
+			car oto(ma, ten, hang, color, hp, gia);
+			xehoi.push_back(oto);
+		}
+	}
+	cin.ignore();
+	string s;
+	cin >> s;
+	cout << "DANH SACH OTO : " << endl;
+	for (auto x : xehoi)
+	{
+		if (x.getTen() == s)
+		{
+			x.in();
+		}
+	}
+	cout << "DANH SACH XE MAY : " << endl;
+	for (auto x : xemay)
+	{
+		if (x.getTen() == s)
+		{
+			x.in();
+		}
+	}
+}
