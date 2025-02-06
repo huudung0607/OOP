403853e72d910ec6930a3eef505c68f152ba018b
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-02-06 20:12:32 +0700

    Create Nhân Viên 1.cpp

---
+++ Nhân Viên 1.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+class nhanvien
+{
+private:
+	int ma;
+	string hoten, gt, ns, dc, mst, ngayhd;
+public:
+	nhanvien() {}
+	nhanvien(int ma, string hoten, string gt, string ns, string dc, string mst, string ngayhd)
+	{
+		this->ma = ma;
+		this->hoten = hoten;
+		this->gt = gt;
+		this->ns = ns;
+		this->dc = dc;
+		this->mst = mst;
+		this->ngayhd = ngayhd;
+	}
+	void chuanhoaNS() {
+		if (ns[1] == '/')
+		{
+			ns = "0" + ns;
+		}
+		if (ns[4] == '/')
+		{
+			ns.insert(3, "0");
+		}
+	}
+	void chuanhoaHD()
+	{
+		if (ngayhd[1] == '/')
+		{
+			ngayhd = "0" + ngayhd;
+		}
+		if (ngayhd[4] == '/')
+		{
+			ngayhd.insert(3, "0");
+		}
+	}
+	void chuanhoaTen()
+	{
+		stringstream ss(hoten);
+		string temp, res = "";
+		while (ss >> temp)
+		{
+			res += toupper(temp[0]);
+			for (int i = 1; i < temp.size(); i++)
+			{
+				res += tolower(temp[i]);
+			}
+			res += " ";
+		}
+		res.pop_back();
+		hoten = res;
+	}
+	friend ostream& operator << (ostream& out, nhanvien x)
+	{
+		out << setw(5) << setfill('0') << x.ma << " " << x.hoten << " " << x.gt << " "
+		<< x.ns << " " << x.dc << " " << x.mst << " " << x.ngayhd << endl;
+		return out;
+	}
+};
+int main()
+{
+	int n;
+	cin >> n;
+	cin.ignore();
+	vector<nhanvien> v;
+	for (int i = 1; i <= n; i++)
+	{
+		string hoten, gt, ns, dc, mst, ngayhd;
+		getline(cin, hoten);
+		getline(cin, gt);
+		getline(cin, ns);
+		getline(cin, dc);
+		getline(cin, mst);
+		getline(cin, ngayhd);
+		nhanvien nv(i, hoten, gt, ns, dc, mst, ngayhd);
+		nv.chuanhoaNS();
+		nv.chuanhoaHD();
+		v.push_back(nv);
+	}
+	for (auto x : v)
+	{
+		cout << x;
+	}
+	
+}
